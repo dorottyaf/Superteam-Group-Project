ecd9814d23ca8a476e2ad278ef17e91a145451b4
Echo Nattinger <echonattinger@utexas.edu>
2024-02-14 17:24:59 -0600

    pop vars, file names, geopandas

--- generate.py
+++ generate.py
@@ -64,6 +64,11 @@ educ_2009_vars = tuple(less_than + hs + some_coll + bachelors + grad)
 
 income_2009_vars = ("NAME", "B08119_002E", "B08119_003E", "B08119_004E", "B08119_005E", "B08119_006E", "B08119_007E", "B08119_008E", "B08119_009E")
 
+# These are defined separately because the years have different variable codings
+# Will be joined with respective datasets in cleaning
+population_0914 = ("NAME", "B00001_001E")
+population_1922 = ("NAME", "B01003_001E")
+
 make_pull(2019, gender_vars, "data/raw_data/2015-2019/gender_2015-2019.csv")
 make_pull(2019, race_vars, "data/raw_data/2015-2019/race_2015-2019.csv")
 make_pull(2019, age_vars, "data/raw_data/2015-2019/age_2015-2019.csv")
@@ -71,6 +76,7 @@ make_pull(2019, educ_vars, "data/raw_data/2015-2019/educ_2015-2019.csv")
 make_pull(2019, income_vars, "data/raw_data/2015-2019/income_2015-2019.csv")
 make_pull(2019, household_vars, "data/raw_data/2015-2019/household_2015-2019.csv")
 make_pull(2019, ethnicity_vars, "data/raw_data/2015-2019/ethnicity_2015-2019.csv")
+make_pull(2019, population_1922, "data/raw_data/2015-2019/population_2015-2019.csv")
 
 make_pull(2014, gender_vars, "data/raw_data/2010-2014/gender_2010-2014.csv")
 make_pull(2014, race_vars, "data/raw_data/2010-2014/race_2010-2014.csv")
@@ -79,6 +85,7 @@ make_pull(2014, educ_vars, "data/raw_data/2010-2014/educ_2010-2014.csv")
 make_pull(2014, income_vars, "data/raw_data/2010-2014/income_2010-2014.csv")
 make_pull(2014, household_vars, "data/raw_data/2010-2014/household_2010-2014.csv")
 make_pull(2014, ethnicity_vars, "data/raw_data/2010-2014/ethnicity_2010-2014.csv")
+make_pull(2014, population_0914, "data/raw_data/2010-2014/population_2010-2014.csv")
 
 make_pull(2009, gender_vars, "data/raw_data/2005-2009/gender_2005-2009.csv")
 make_pull(2009, race_vars, "data/raw_data/2005-2009/race_2005-2009.csv")
@@ -87,6 +94,7 @@ make_pull(2009, educ_2009_vars, "data/raw_data/2005-2009/educ_2005-2009.csv")
 make_pull(2009, income_2009_vars, "data/raw_data/2005-2009/income_2005-2009.csv")
 make_pull(2009, household_vars, "data/raw_data/2005-2009/household_2005-2009.csv")
 make_pull(2009, ethnicity_vars, "data/raw_data/2005-2009/ethnicity_2005-2009.csv")
+make_pull(2009, population_0914, "data/raw_data/2005-2009/population_2005-2009.csv")
 
 make_pull_2022(gender_vars, "data/raw_data/2018-2022/gender_2018-2022.csv")
 make_pull_2022(race_vars, "data/raw_data/2018-2022/race_2018-2022.csv")
@@ -95,3 +103,4 @@ make_pull_2022(educ_vars, "data/raw_data/2018-2022/educ_2018-2022.csv")
 make_pull_2022(income_vars, "data/raw_data/2018-2022/income_2018-2022.csv")
 make_pull_2022(household_vars, "data/raw_data/2018-2022/household_2018-2022.csv")
 make_pull_2022(ethnicity_vars, "data/raw_data/2018-2022/ethnicity_2018-2022.csv")
+make_pull_2022(population_1922, "data/raw_data/2018-2022/population_2018-2022")
