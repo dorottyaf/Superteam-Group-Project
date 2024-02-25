51ddd0d6a669f39cb7850181c37dadc7ca8beed0
Echo Nattinger <echonattinger@utexas.edu>
2024-02-25 17:24:34 -0600

    edits to querying module

---
+++ querying/generate.py
@@ -0,0 +1,39 @@
+from APIquery import make_pull, make_pull_2022
+from querying_constants import gender_vars, race_vars, age_vars, educ_vars, income_vars, household_vars, ethnicity_vars, population_1922, population_0914, age_2009_vars, income_2009_vars, educ_2009_vars
+
+# NOTE: Not a function because variable arguments change year by year
+make_pull(2019, gender_vars, "data/raw_data/2015-2019/gender_2015-2019.csv")
+make_pull(2019, race_vars, "data/raw_data/2015-2019/race_2015-2019.csv")
+make_pull(2019, age_vars, "data/raw_data/2015-2019/age_2015-2019.csv")
+make_pull(2019, educ_vars, "data/raw_data/2015-2019/educ_2015-2019.csv")
+make_pull(2019, income_vars, "data/raw_data/2015-2019/income_2015-2019.csv")
+make_pull(2019, household_vars, "data/raw_data/2015-2019/household_2015-2019.csv")
+make_pull(2019, ethnicity_vars, "data/raw_data/2015-2019/ethnicity_2015-2019.csv")
+make_pull(2019, population_1922, "data/raw_data/2015-2019/population_2015-2019.csv")
+
+make_pull(2014, gender_vars, "data/raw_data/2010-2014/gender_2010-2014.csv")
+make_pull(2014, race_vars, "data/raw_data/2010-2014/race_2010-2014.csv")
+make_pull(2014, age_vars, "data/raw_data/2010-2014/age_2010-2014.csv")
+make_pull(2014, educ_vars, "data/raw_data/2010-2014/educ_2010-2014.csv")
+make_pull(2014, income_vars, "data/raw_data/2010-2014/income_2010-2014.csv")
+make_pull(2014, household_vars, "data/raw_data/2010-2014/household_2010-2014.csv")
+make_pull(2014, ethnicity_vars, "data/raw_data/2010-2014/ethnicity_2010-2014.csv")
+make_pull(2014, population_0914, "data/raw_data/2010-2014/population_2010-2014.csv")
+
+make_pull(2009, gender_vars, "data/raw_data/2005-2009/gender_2005-2009.csv")
+make_pull(2009, race_vars, "data/raw_data/2005-2009/race_2005-2009.csv")
+make_pull(2009, age_2009_vars, "data/raw_data/2005-2009/age_2005-2009.csv")
+make_pull(2009, educ_2009_vars, "data/raw_data/2005-2009/educ_2005-2009.csv")
+make_pull(2009, income_2009_vars, "data/raw_data/2005-2009/income_2005-2009.csv")
+make_pull(2009, household_vars, "data/raw_data/2005-2009/household_2005-2009.csv")
+make_pull(2009, ethnicity_vars, "data/raw_data/2005-2009/ethnicity_2005-2009.csv")
+make_pull(2009, population_0914, "data/raw_data/2005-2009/population_2005-2009.csv")
+
+make_pull_2022(gender_vars, "data/raw_data/2018-2022/gender_2018-2022.csv")
+make_pull_2022(race_vars, "data/raw_data/2018-2022/race_2018-2022.csv")
+make_pull_2022(age_vars, "data/raw_data/2018-2022/age_2018-2022.csv")
+make_pull_2022(educ_vars, "data/raw_data/2018-2022/educ_2018-2022.csv")
+make_pull_2022(income_vars, "data/raw_data/2018-2022/income_2018-2022.csv")
+make_pull_2022(household_vars, "data/raw_data/2018-2022/household_2018-2022.csv")
+make_pull_2022(ethnicity_vars, "data/raw_data/2018-2022/ethnicity_2018-2022.csv")
+make_pull_2022(population_1922, "data/raw_data/2018-2022/population_2018-2022")
