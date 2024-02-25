1754956e1781e6f7b533661244d615a5005ef0de
Andrew Baker <andrewbaker@Andrews-MacBook-Air.local>
2024-02-24 18:55:54 -0600

    more vis uploads, getting shape files and such

--- visualization/explore.py
+++ visualization/explore.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import geopandas as gpd
 import pathlib
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -6,6 +7,7 @@ import pprint
 
 variables = ["age", "ethnicity", "household", "educ", "gender", "income", "race"]
 COLS_TO_DROP = ['NAME', 'state', 'county', 'tract', 'population', 'period']
+COLS_TO_DROP2 = ['NAME', 'state', 'county', 'tract', 'population']
 age_top_10 = [835700, 611800, 834600, 670100, 251700, 835600, 690500, 380500, 830201, 841400]
 eth_top_10 = []
 hh_top_10 = []
@@ -20,7 +22,8 @@ def load_data(name:str):
     filename = pathlib.Path(__file__).parent.parent / "data" / "clean_data" / csv_name
     data = pd.read_csv(filename)
     return data
-
+"""
+# for exporatory, getting tracts with greatest changes for each of 7 variables
 for var in variables:
     df = load_data(var)
     group = df.groupby("tract")
@@ -40,3 +43,28 @@ for var in variables:
                 direc = pathlib.Path(__file__).parent / 'temp_graphs' / png_name
                 plt.savefig(direc)
                 plt.clf()
+                """
+
+def get_cook_shape():
+    shp_file = "tl_rd22_17_tract.zip"
+    shp_file_path = pathlib.Path(__file__).parent / "shapes" / shp_file
+    il_tract = gpd.read_file(shp_file_path)
+    il_tract = il_tract.to_crs(epsg = 32617)
+    mask = il_tract['COUNTYFP'] == '031'
+    cook_tract = il_tract[mask]
+    return cook_tract
+
+
+
+cook_tract = get_cook_shape()
+for var in variables:
+    df = load_data(var)
+    df["GEOID"] = df['state'].astype(str) + '0' + df['county'].astype(str) + df['tract'].astype(str)
+    df = df.drop(columns= COLS_TO_DROP2)
+    df_merge = df.merge(cook_tract, on= 'GEOID', how= 'left')
+    if var == 'age':
+        print(cook_tract.head(2))
+        print('Shape, ', cook_tract.shape)
+        print("\nThe shapefile projection is: {}".format(cook_tract.crs))
+        print(df)
+        print(df_merge)
