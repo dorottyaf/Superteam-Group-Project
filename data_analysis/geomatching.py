f0e8e4aed54e5003bb4549e62007796ad046dcf4
unknown <pjsoltys@uchicago>
2024-02-25 18:12:57 -0600

    shape_matcher is now FLEXIBLER

--- data_analysis/geomatching.py
+++ data_analysis/geomatching.py
@@ -9,24 +9,35 @@ shapes being rolled up, and Community Area as the larger shape being rolled into
 '''
 
 #Path Variables for shapefiles
-TRACT_2000 = pathlib.Path(__file__).parent / "Location Information" / "Boundaries - Census Tracts - 2000" / "geo_export_c39c40c3-f0d6-44b1-b60d-c608f5f21ffe.shp" 
-TRACT_2010 = pathlib.Path(__file__).parent / "Location Information" / "tl_2010_17_tract" / "tl_2018_17_tract.shp"
-TRACT_2020 = pathlib.Path(__file__).parent / "Location Information" / "tl_2020_17_tract" / "tl_2020_17_tract.shp"
-ZIP_CODES = pathlib.Path(__file__).parent / "Location Information" / "Boundaries - ZIP Codes" / "geo_export_0ee546b2-a3fb-4bdb-8cc1-febaad94a4d8.shp"
+PATH_2000 = pathlib.Path(__file__).parent / "Location Information" / "Boundaries - Census Tracts - 2000" / "geo_export_c39c40c3-f0d6-44b1-b60d-c608f5f21ffe.shp" 
+PATH_2010 = pathlib.Path(__file__).parent / "Location Information" / "tl_2010_17_tract" / "tl_2018_17_tract.shp"
+PATH_2020 = pathlib.Path(__file__).parent / "Location Information" / "tl_2020_17_tract" / "tl_2020_17_tract.shp"
+PATH_ZIP = pathlib.Path(__file__).parent / "Location Information" / "Boundaries - ZIP Codes" / "geo_export_0ee546b2-a3fb-4bdb-8cc1-febaad94a4d8.shp"
+
 COMM_AREAS = pathlib.Path(__file__).parent / "Location Information" / "Boundaries - Community Areas (current)" / "geo_export_8fac6090-b29a-4cf4-b6ab-c66b0d4da44a.shp" 
+
+#Name, Path Tuples used to run shape_matcher
+TRACT_2000 = ("census_tra", PATH_2000,) 
+TRACT_2010 = ("TRACTCE", PATH_2010,)
+TRACT_2020 = ("TRACTCE", PATH_2020,)
+ZIP_CODES =  ("zip", PATH_ZIP,)
 
 
 def shape_matcher(small_name, large_name):
     '''
     Inputs: 
-        small_df: name of smaller set of shapes, giving the global path variable 
-        large_df: name of larger set of shapes, giving the global path variable 
+        small_name: name of smaller set of shapes, a tuple w/ column name and
+            the path variable.
+        large_name: name of larger set of shapes- should be COMM_AREAS always
+            currently, but set up to allow other large areas if needed.
+    
+    Example call: shape_dict = shape_matcher(TRACT_2010, COMM_AREAS)
 
     Returns:
         comm_area_dict: Dictionary where keys are the larger shape, and each value is
         a list of smaller shapes that will be included in 
     '''
-    small_df = gpd.read_file(small_name)
+    small_df = gpd.read_file(small_name[1])
     large_df = gpd.read_file(large_name)
     large_df = large_df.to_crs(3857)
     small_df = small_df.to_crs(3857)
@@ -43,7 +54,8 @@ def shape_matcher(small_name, large_name):
         intersection_areas = intersections.area
         for large_ind in large_df.index:
             if intersection_areas[large_ind] > (small_areas[small_ind] / 2):
-                comm_area_dict[large_df["community"][large_ind]].append(small_df["TRACTCE"][small_ind])
+                comm_area_dict[large_df["community"][large_ind]].append(
+                    small_df[small_name[0]][small_ind])
                 break
 
     return comm_area_dict
